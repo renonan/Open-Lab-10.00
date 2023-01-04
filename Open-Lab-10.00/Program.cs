3568ef1deac44826d1db9462d51224276ab98d4b
Roman <vojtus.roman@gmail.com>
2023-01-04 22:17:03 +0100

    10.04-Fixed mistake of not using method for writing out objects into console.

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -20,11 +20,11 @@ namespace Open_Lab_10._00
             LOTR.ReleaseDate=1954;
             Book LOTR2 = new Book();
             Book LOTR3 = new Book("LOTR",416);
-            Book HOBIT = new Book("Hobbit", 310, "Fantasy", "J.R.R.Tolkien",1937) ;
-            Console.WriteLine($"Title:{LOTR.Title} \n Pages:{LOTR.Pages}\n Category:{LOTR.Category}\n Author:{LOTR.Author}\n ReleaseDate:{LOTR.ReleaseDate}\n");
-            Console.WriteLine($"Title:{LOTR2.Title} \n Pages:{LOTR2.Pages}\n Category:{LOTR2.Category}\n Author:{LOTR2.Author}\n ReleaseDate:{LOTR2.ReleaseDate}\n");
-            Console.WriteLine($"Title:{LOTR3.Title} \n Pages:{LOTR3.Pages}\n Category:{LOTR3.Category}\n Author:{LOTR3.Author}\n ReleaseDate:{LOTR3.ReleaseDate}\n");
-            Console.WriteLine($"Title:{HOBIT.Title} \n Pages:{HOBIT.Pages}\n Category:{HOBIT.Category}\n Author:{HOBIT.Author}\n ReleaseDate:{HOBIT.ReleaseDate}\n");
+            Book HOBBIT = new Book("Hobbit", 310, "Fantasy", "J.R.R.Tolkien",1937) ;
+            LOTR.WriteInfo();
+            LOTR2.WriteInfo();
+            LOTR3.WriteInfo();
+            HOBBIT.WriteInfo();
         }
     }
 }
